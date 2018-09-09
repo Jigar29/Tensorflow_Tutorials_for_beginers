98980a8e8ec27ba7b6c88f4593a8c9a387d8936e
Jigar <jigaragrawal29@gmail.com>
2018-09-09 16:59:39 -0700

    The graphs file added and the gitignore file updated to ignore log file getting commited

---
+++ src/graphs.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+g1 = tf.Graph()
+with g1.as_default():
+    a = tf.constant(8)
+    b = tf.constant(2)
+    op = tf.add(a, b, name='Graph2_operation')
+
+g2 = tf.Graph()
+with g2.as_default():
+    a = tf.constant(2)
+    b = tf.constant(2)
+    op2 = tf.add(a,b, name='Graph2_operation')
+
+with tf.Session(graph=g1) as sess1:
+    print(sess1.run(op))
+
+with tf.Session(graph=g2) as sess2:
+    print(sess2.run(op2))
